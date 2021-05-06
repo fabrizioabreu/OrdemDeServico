02b8f92c51e15447731482c9585582221b31c570
Fabrizio Abreu <fabrizioabreu@live.com>
2021-05-06 10:12:20 -0300

    Iniciando Projeto

---
+++ src/main/java/com/fabrizio/os/OrdemDeServicoApplication.java
@@ -0,0 +1,13 @@
+package com.fabrizio.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemDeServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemDeServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrizio/os/OrdemDeServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrizio.os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemDeServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
