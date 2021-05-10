dcc569b81932cfa4aafb3ca8e62d34ed12e16bb8
Fabrizio Abreu <fabrizioabreu@live.com>
2021-05-10 19:48:46 -0300

    Exceção para Objeto não encontrado

---
+++ src/main/java/com/fabrizio/os/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.fabrizio.os.controllers.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.fabrizio.os.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e) {
+		StandardError error = new StandardError(System.currentTimeMillis(), 
+				HttpStatus.NOT_FOUND.value(), e.getMessage());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+}
--- src/main/java/com/fabrizio/os/services/TecnicoService.java
+++ src/main/java/com/fabrizio/os/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fabrizio.os.domain.Tecnico;
 import com.fabrizio.os.repositories.TecnicoRepository;
+import com.fabrizio.os.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Tecnico.class.getName()));
 	}
 }
