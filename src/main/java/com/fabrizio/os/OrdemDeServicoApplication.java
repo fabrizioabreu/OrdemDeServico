23ca85181f29940a12ab3ba43814423893f06713
Fabrizio Abreu <fabrizioabreu@live.com>
2021-05-10 17:20:42 -0300

    Criando Perfil de Teste

--- src/main/java/com/fabrizio/os/OrdemDeServicoApplication.java
+++ src/main/java/com/fabrizio/os/OrdemDeServicoApplication.java
@@ -1,51 +1,13 @@
 package com.fabrizio.os;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fabrizio.os.domain.Cliente;
-import com.fabrizio.os.domain.OS;
-import com.fabrizio.os.domain.Tecnico;
-import com.fabrizio.os.domain.enuns.Prioridade;
-import com.fabrizio.os.domain.enuns.Status;
-import com.fabrizio.os.repositories.ClienteRepository;
-import com.fabrizio.os.repositories.OSRepository;
-import com.fabrizio.os.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class OrdemDeServicoApplication implements CommandLineRunner{
+public class OrdemDeServicoApplication {
 	
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private OSRepository oSRepository;
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrdemDeServicoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Tecnico t1 = new Tecnico(null, "Fabrizio Abreu", "623.813.710-01", "(13) 99999-8888");
-		Cliente c1 = new Cliente(null, "Betina Campos", "019.065.820-76", "(13) 99999-7777");
-		OS os1 = new OS(null, Prioridade.ALTA, "Teste create OS", Status.ANDAMENTO, t1, c1);
-		
-		t1.getList().add(os1);
-		c1.getList().add(os1);
-		
-		tecnicoRepository.saveAll(Arrays.asList(t1));
-		clienteRepository.saveAll(Arrays.asList(c1));
-		oSRepository.saveAll(Arrays.asList(os1));
 	}
 
 }
---
+++ src/main/java/com/fabrizio/os/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.fabrizio.os.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.fabrizio.os.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
+
+}
